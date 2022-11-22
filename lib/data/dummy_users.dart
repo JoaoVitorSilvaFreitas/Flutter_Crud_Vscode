138337225603caffd1dec84e05c7ffe5cd5f714b
JoaoVitorSilvaFreitas <joaovitordasilvafreitas@gmail.com>
2022-11-22 12:09:47 -0300

    Implementação de modelo de usuário

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import '../models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'maria',
+      email: 'maria@joana.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Clara',
+      email: 'Clara@joana.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2022/10/04/07/23/alien-7497513_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'jhon',
+      email: 'jhon@joana.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png')
+};
